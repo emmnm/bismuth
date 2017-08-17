2756e77ec530dd29df27b3ac5c325f52910a71c7
Michael Meyer <meyerz.michael@gmail.com>
2017-08-17 14:49:36 -0500

    Pull out vertex generation code to separate method

--- src/main.rs
+++ src/main.rs
@@ -7,6 +7,14 @@ mod shaders;
 use geometry::Vertex;
 use shaders::{VERTEX_SHADER_SRC, FRAGMENT_SHADER_SRC};
 
+fn get_vertices() -> Vec<Vertex> {
+    let vertex1 = Vertex { position: [-0.5, -0.5] };
+    let vertex2 = Vertex { position: [0.0, 0.5] };
+    let vertex3 = Vertex { position: [0.5, -0.25] };
+    let vertex4 = Vertex { position: [0.5, 0.0] };
+    vec![vertex1, vertex2, vertex3, vertex4]
+}
+
 fn main() {
     use glium::{glutin, Surface};
 
@@ -15,13 +23,10 @@ fn main() {
     let context = glutin::ContextBuilder::new();
     let display = glium::Display::new(window, context, &events_loop).unwrap();
 
-    let vertex1 = Vertex { position: [-0.5, -0.5] };
-    let vertex2 = Vertex { position: [0.0, 0.5] };
-    let vertex3 = Vertex { position: [0.5, -0.25] };
-    let shape = vec![vertex1, vertex2, vertex3];
+    let shape = get_vertices();
 
     let vertex_buffer = glium::VertexBuffer::new(&display, &shape).unwrap();
-    let indices = glium::index::NoIndices(glium::index::PrimitiveType::TrianglesList);
+    let indices = glium::index::NoIndices(glium::index::PrimitiveType::LinesList);
 
     let program =
         glium::Program::from_source(&display, VERTEX_SHADER_SRC, FRAGMENT_SHADER_SRC, None)
@@ -30,7 +35,7 @@ fn main() {
     let mut closed = false;
     while !closed {
         let mut target = display.draw();
-        target.clear_color(0.0, 0.0, 1.0, 1.0);
+        target.clear_color(0.0, 0.0, 0.05, 1.0);
         target
             .draw(
                 &vertex_buffer,
